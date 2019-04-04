b37ab09124eb8c03bbd79b4e1033548f59b0ae81
Won Kim <wonsae.kim@gmail.com>
2019-04-04 03:11:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   while !array.empty?
     msg += array.shift
     
-    if array.length != 1
+    if array.length != 1 || array.length == 0
       msg += " and "
     end
   end
